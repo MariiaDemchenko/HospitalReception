5613a8c8e2f067a5667c267cd52dcd1cb7f32c48
Maria Demchenko <maria.demchenko@dataart.com>
2018-04-09 13:19:35 +0300

    Task 3. Added missing file

---
+++ PhotoManager/PhotoManager/ViewModels/PhotoManagerViewModels/DeleteModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace PhotoManager.ViewModels.PhotoManagerViewModels
+{
+    public class DeleteModel
+    {
+        public IEnumerable<int> PhotosId { get; set; }
+        public int AlbumId { get; set; }
+    }
+}
