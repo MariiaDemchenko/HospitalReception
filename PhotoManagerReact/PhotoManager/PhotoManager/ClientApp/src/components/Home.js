b9eaffa2eadaa0c90fd332416fed583ac04efb95
Maria Demchenko <mdemchenko@collettevacations.com>
2019-04-10 11:30:07 +0300

    Added readme and images for testing

--- PhotoManagerReact/PhotoManager/PhotoManager/ClientApp/src/components/Home.js
+++ PhotoManagerReact/PhotoManager/PhotoManager/ClientApp/src/components/Home.js
@@ -4,94 +4,94 @@ import { connect } from 'react-redux';
 
 const images =
     [{
-        src: "http://localhost/images/black.jpg",
-        thumbnail: "http://localhost/thumbs/black.jpg",
+        src: "http://mdemchenko.vrn.dataart.net/images/black.jpg",
+        thumbnail: "http://mdemchenko.vrn.dataart.net/thumbs/black.jpg",
         thumbnailWidth: 320,
         thumbnailHeight: 174,
         caption: "Foggy Lake Photo"
     },
     {
-        src: "http://localhost/images/blue.jpg",
-        thumbnail: "http://localhost/thumbs/blue.jpg",
+        src: "http://mdemchenko.vrn.dataart.net/images/blue.jpg",
+        thumbnail: "http://mdemchenko.vrn.dataart.net/thumbs/blue.jpg",
         thumbnailWidth: 320,
         thumbnailHeight: 212,
         caption: "Strawberry on blue surface"
     },
 
     {
-        src: "http://localhost/images/yellow.jpg",
-        thumbnail: "http://localhost/thumbs/yellow.jpg",
+        src: "http://mdemchenko.vrn.dataart.net/images/yellow.jpg",
+        thumbnail: "http://mdemchenko.vrn.dataart.net/thumbs/yellow.jpg",
         thumbnailWidth: 320,
         thumbnailHeight: 212
     },
 
     {
-        src: "http://localhost/images/brown.jpg",
-        thumbnail: "http://localhost/images/brown.jpg",
+        src: "http://mdemchenko.vrn.dataart.net/images/brown.jpg",
+        thumbnail: "http://mdemchenko.vrn.dataart.net/images/brown.jpg",
         thumbnailWidth: 320,
         thumbnailHeight: 212
     },
 
     {
-        src: "http://localhost/images/clearLake.jpg",
-        thumbnail: "http://localhost/images/clearLake.jpg",
+        src: "http://mdemchenko.vrn.dataart.net/images/clearLake.jpg",
+        thumbnail: "http://mdemchenko.vrn.dataart.net/images/clearLake.jpg",
         thumbnailWidth: 320,
         thumbnailHeight: 212
     },
 
     {
-        src: "http://localhost/images/coffee.jpg",
-        thumbnail: "http://localhost/images/coffee.jpg",
+        src: "http://mdemchenko.vrn.dataart.net/images/coffee.jpg",
+        thumbnail: "http://mdemchenko.vrn.dataart.net/images/coffee.jpg",
         thumbnailWidth: 320,
         thumbnailHeight: 212
     },
 
     {
-        src: "http://localhost/images/colors.jpg",
-        thumbnail: "http://localhost/images/colors.jpg",
+        src: "http://mdemchenko.vrn.dataart.net/images/colors.jpg",
+        thumbnail: "http://mdemchenko.vrn.dataart.net/images/colors.jpg",
         thumbnailWidth: 320,
         thumbnailHeight: 212
     },
 
     {
-        src: "http://localhost/images/darkblue.jpg",
-        thumbnail: "http://localhost/images/darkblue.jpg",
+        src: "http://mdemchenko.vrn.dataart.net/images/darkblue.jpg",
+        thumbnail: "http://mdemchenko.vrn.dataart.net/images/darkblue.jpg",
         thumbnailWidth: 320,
         thumbnailHeight: 212
     },
 
     {
-        src: "http://localhost/images/desert.jpg",
-        thumbnail: "http://localhost/images/desert.jpg",
+        src: "http://mdemchenko.vrn.dataart.net/images/desert.jpg",
+        thumbnail: "http://mdemchenko.vrn.dataart.net/images/desert.jpg",
         thumbnailWidth: 320,
         thumbnailHeight: 212
     },
 
     {
-        src: "http://localhost/images/dessert.jpg",
-        thumbnail: "http://localhost/images/dessert.jpg",
+        src: "http://mdemchenko.vrn.dataart.net/images/dessert.jpg",
+        thumbnail: "http://mdemchenko.vrn.dataart.net/images/dessert.jpg",
         thumbnailWidth: 320,
         thumbnailHeight: 212
     }
         ,
 
     {
-        src: "http://localhost/images/forest.jpg",
-        thumbnail: "http://localhost/images/forest.jpg",
+        src: "http://mdemchenko.vrn.dataart.net/images/forest.jpg",
+        thumbnail: "http://mdemchenko.vrn.dataart.net/images/forest.jpg",
         thumbnailWidth: 320,
         thumbnailHeight: 212
     },
 
     {
-        src: "http://localhost/images/gray.jpg",
-        thumbnail: "http://localhost/images/gray.jpg",
+        src: "http://mdemchenko.vrn.dataart.net/images/gray.jpg",
+        thumbnail: "http://mdemchenko.vrn.dataart.net/images/gray.jpg",
         thumbnailWidth: 320,
         thumbnailHeight: 212
     },
 
     {
-        src: "http://localhost/images/green.jpg",
-        thumbnail: "http://localhost/images/green.jpg",
+        src: "http://mdemchenko.vrn.dataart.net/images/green.jpg",
+        thumbnail: "http://mdemchenko.vrn.dataart.net/images/green.jpg",
         thumbnailWidth: 320,
         thumbnailHeight: 212
     }
